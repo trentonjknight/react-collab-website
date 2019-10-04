03622aeee21e38fd26cdb52c02769025a4f704f4
Trenton Knight <tknightj1995@yahoo.com>
2019-10-04 16:36:30 +0000

    style

--- victors-shoes/src/Components/Vshoes.js
+++ victors-shoes/src/Components/Vshoes.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Vshoes(){
     return(
-        <div className="jumbotron jumbotron-fluid">
+        <div className="jumbotron jumbotron-fluid bg-light text-info">
             <div className="container">
                 <h1 className="display-4">Victor's Everyday Footwear</h1>
                     <p className="lead">Walk comfortably in our endless selection of footwear styles for the adventurer in you!</p>
