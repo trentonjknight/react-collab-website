0216d787c967e9fe58c00e1e7509d753bbeee58b
samir benzada <samirbenzada@gmail.com>
2019-10-04 17:11:19 -0400

    popup

--- victors-shoes/src/Components/Popup.js
+++ victors-shoes/src/Components/Popup.js
@@ -3,23 +3,39 @@ import '../App.css';
 
 const Popup = () => {
 
-const [count, setCount] = useState('');
+const [flash, setFlash] = useState('');
+// const [count, setCount] = useState(0);
+
 
 useEffect(() => {
-    const timer = setTimeout(() => {
-        setCount(
-            <div className="popup">
-                <h1>Timeout called!</h1>
-            </div>
+    setTimeout(() => {
+        setFlash(
+                <div className="popup card">
+                    <div className="card-header">
+                        <div class="alert alert-warning" role="alert">
+                        Flash Sale<span className="off float-right">50% OFF</span>
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">Victor's Left Shoe</h5>
+                        <p className="card-text">We are selling the most famous shoe of victor, <br/>the left one because the right one has been eaten by his dog.</p>
+                        <p className="card-text">Sale Ends in 60 seconds</p>
+                        <a href="/" className="btn btn-primary">Buy Now!</a>
+                    </div>
+                </div>
         );
-    }, 3000);
-    return () => clearTimeout(timer);
+    }, 10000);
+    setTimeout(() => {
+        setFlash('');
+    },70000);
+    // return () => clearTimeout(timer);
+    // setInterval(count => setCount(count++), 1000);
     }, []);
 
 
     return (
         <>
-        {count}
+        {flash}
         </>
     );
 };
