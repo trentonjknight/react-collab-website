7ce42cb89ab53aa8dac9a765d4d2c59ae9803eb4
Trenton Knight <tknightj1995@yahoo.com>
2019-10-07 20:38:32 +0000

    cleaned up

--- victors-shoes/src/Components/Carousel.js
+++ victors-shoes/src/Components/Carousel.js
@@ -3,27 +3,39 @@ import React from 'react';
 
 function Carousel(){
     return(
-            <div id="carouselExampleControls" className="carousel slide mb-5" data-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src="https://stockx.imgix.net/Adidas-JS-Wings-Solid-Gold.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&trim=color&updated_at=1538080256&w=1000" className="d-block w-50" alt="..."></img>
+        <div id="carouselExampleControls" className="carousel slide mb-5" data-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="https://stockx.imgix.net/Adidas-JS-Wings-Solid-Gold.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&trim=color&updated_at=1538080256&w=1000" className="d-block w-50" alt="..."></img>
+                    <div class="carousel-caption position-absolute text-dark" style={{bottom:"-35px"}}>
+                        <h5> Gold sneakers you need!</h5>
+                        <p>Pull up on the squad looking extra fly.</p>
+                    </div>
+            </div>
+            <div className="carousel-item">
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/81e-ZZIZ8hL._UL1500_.jpg" className="d-block w-50" alt="..."></img>
+                    <div class="carousel-caption text-dark position-absolute" style={{bottom:"-36px"}}>
+                        <h5> Nobody is too good for cheap shoes!</h5>
+                        <p>Dads everywhere will appreciate the utility of the ugliest shoes you can buy!</p>
+                    </div>
+            </div>
+            <div className="carousel-item">
+                    <img src="https://stockx.imgix.net/adidas-Yeezy-Boost-350-V2-Static-Black-Reflective-1-1.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1560787762" className="d-block w-50" alt="..."></img>
+                    <div class="carousel-caption text-dark position-absolute" style={{bottom:"-35px"}}>
+                        <h5> Look like you're homeless in these monstrosities!</h5>
+                        <p>Show off your style to the homies and get mad respect.</p>
+                    </div>
+            </div>
+            </div>
+            <a className="carousel-control-prev bg-light" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next bg-light" href="#carouselExampleControls" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+            </a>
         </div>
-                <div className="carousel-item">
-                        <img src="https://images.complex.com/complex/images/c_limit,dpr_auto,q_90,w_720/fl_lossy,pg_1/y1gdarwadxuyvv66zqwj/new-balance-990-v5" className="d-block w-50" alt="..."></img>
-        </div>
-                <div className="carousel-item">
-                       <img src="https://i.ytimg.com/vi/3CMvtxUmOD0/maxresdefault.jpg" className="d-block w-50" alt="..."></img>
-        </div>
-    </div>
-                <a className="carousel-control-prev bg-light" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                     <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next bg-light" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
-                </a>
-    </div>
     )
 }
 
