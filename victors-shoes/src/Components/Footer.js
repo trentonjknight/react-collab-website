4f239d728ab7a3261441b4615c408ad012e67c4d
samir benzada <samirbenzada@gmail.com>
2019-10-07 14:32:36 -0400

    footer color

--- victors-shoes/src/Components/Footer.js
+++ victors-shoes/src/Components/Footer.js
@@ -5,45 +5,47 @@ import Modal from './Modal';
 
 const Footer = () => {
     return (
-        <div className="container-fluid">
-        <footer className="pt-4 my-md-5 pt-md-5 border-top">
-            <div className="row">
-            <div className="col-12 col-md">
-                <img className="mb-2 rounded" src={VicPicture} alt="" width="100" height="100" />
-                <small className="d-block mb-3 text-muted">© 2019-2020</small>
+        <div className="navBarEntire mt-5">
+            <div className="container-fluid">
+            <footer className="pt-5 border-top">
+                <div className="row">
+                <div className="col-12 col-md">
+                    <img className="mb-2 rounded" src={VicPicture} alt="" width="100" height="100" />
+                    <small className="d-block mb-3 ">© 2019-2020</small>
+                </div>
+                <div className="col-6 col-md">
+                    <h5>Features</h5>
+                    <ul className="list-unstyled text-small">
+                    <Modal/>
+                    <li><a href="/">Cool stuff</a></li>
+                    <li><a href="/">Random feature</a></li>
+                    <li><a href="/">Team feature</a></li>
+                    <li><a href="/">Stuff for developers</a></li>
+                    <li><a href="/">Another one</a></li>
+                    <li><a href="/">Last time</a></li>
+                    </ul>
+                </div>
+                <div className="col-6 col-md">
+                    <h5>Resources</h5>
+                    <ul className="list-unstyled text-small">
+                    <li><a href="/">Resource</a></li>
+                    <li><a href="/">Resource name</a></li>
+                    <li><a href="/">Another resource</a></li>
+                    <li><a href="/">Final resource</a></li>
+                    </ul>
+                </div>
+                <div className="col-6 col-md">
+                    <h5>About</h5>
+                    <ul className="list-unstyled text-small">
+                    <li><a href="/">Team</a></li>
+                    <li><a href="/">Locations</a></li>
+                    <li><a href="/">Privacy</a></li>
+                    <li><a href="/">Terms</a></li>
+                    </ul>
+                </div>
+                </div>
+            </footer>
             </div>
-            <div className="col-6 col-md">
-                <h5>Features</h5>
-                <ul className="list-unstyled text-small">
-                <Modal/>
-                <li><a className="text-muted" href="/">Cool stuff</a></li>
-                <li><a className="text-muted" href="/">Random feature</a></li>
-                <li><a className="text-muted" href="/">Team feature</a></li>
-                <li><a className="text-muted" href="/">Stuff for developers</a></li>
-                <li><a className="text-muted" href="/">Another one</a></li>
-                <li><a className="text-muted" href="/">Last time</a></li>
-                </ul>
-            </div>
-            <div className="col-6 col-md">
-                <h5>Resources</h5>
-                <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="/">Resource</a></li>
-                <li><a className="text-muted" href="/">Resource name</a></li>
-                <li><a className="text-muted" href="/">Another resource</a></li>
-                <li><a className="text-muted" href="/">Final resource</a></li>
-                </ul>
-            </div>
-            <div className="col-6 col-md">
-                <h5>About</h5>
-                <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="/">Team</a></li>
-                <li><a className="text-muted" href="/">Locations</a></li>
-                <li><a className="text-muted" href="/">Privacy</a></li>
-                <li><a className="text-muted" href="/">Terms</a></li>
-                </ul>
-            </div>
-            </div>
-        </footer>
         </div>
     );
 };
